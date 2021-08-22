f880e18be4df95f6279bba33f7a09cf6cf683784
lucasbdassis <lucas-b-assis@hotmail.com>
2021-08-22 13:54:58 -0300

    funcional

--- models/model.go
+++ models/model.go
@@ -13,13 +13,14 @@ type Product struct {
 	Size        string         `json:"size"`
 	Description string         `json:"description"`
 	MediumPrice float32        `json:"medium_price"`
-	Pasta       Pasta          `json:"pasta" gorm:"-"`
+	//Pasta       Pasta          `json:"pasta" gorm:"-"`
 	ImageURL    string         `json:"img_url"`
 	CreatedAt   time.Time      `json:"created"`
 	UpdatedAt   time.Time      `json:"updated"`
 	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted"`
 }
-
+/*
 type Pasta struct {
 	Traditional *[]string `json:"traditional"`
 }
+*/
